6b60133608cc1ff73f6a3ecd739370ccdc4bbc21
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2023-02-14 23:25:51 -0500

    Sincronizacion Rutinaria

--- 5. PHP/programa.php
+++ 5. PHP/programa.php
@@ -1,5 +1,5 @@
 <?php
-    $saludo = 'Hola Mundo!';
+    declare(strict_types = 1);
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +8,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Programa</title>
 </head>
 <body>
-    <p><?php print($saludo) ?></p>
+    <p>
+        <?php
+            
+        ?>
+    </p>
 </body>
 </html>
