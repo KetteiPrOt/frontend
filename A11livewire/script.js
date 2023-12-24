350eb8edeb30e823610999f962aa6a6a47edb645
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2023-12-24 00:31:03 -0500

    Livewire Class 1 finalized

---
+++ A11livewire/script.js
@@ -0,0 +1,23 @@
+let chaptersText = [
+    'nombreCapitulo1'
+];
+
+let partsText = [
+    'Introducción',
+    'Renderizar componentes'
+];
+
+let chapter, part;
+
+for(let i = 0; i < chaptersText.length; i++){
+    chapter = document.getElementById(`c${i + 1}`);
+    for(let j = 0; j < partsText.length; j++){
+        part = document.getElementById(`c${i + 1}p${j + 1}`);
+        if(chapter){
+            chapter.textContent = chaptersText[i];
+        }
+        if(part){
+            part.textContent = partsText[j];
+        }
+    }
+}
