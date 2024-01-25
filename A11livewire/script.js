3deb176aaf155a6a084ba1b0ce9cdabb2979a8ab
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-01-25 12:11:16 -0500

    Finalized Livewire Chapters 1 and 2

--- A11livewire/script.js
+++ A11livewire/script.js
@@ -1,22 +1,38 @@
 let chaptersText = [
-    'Componentes'
+    'Componentes',
+    'Propiedades'
 ];
 
 let partsText = [
-    'Creando componentes'
+    // Cap 1
+    [
+        'Creando componentes',
+        'Estableciendo propiedades',
+        'Llamando acciones',
+        'Renderizando componentes',
+        'Componentes de pagina completa',
+        'Usando JavaScript'
+    ],
+    // Cap 2
+    [
+        'Manipulación de propiedades',
+        'Tipos de propiedades soportadas',
+        'Accedienso a propiedades desde JavaScript',
+        'Precauciones de seguridad'
+    ],
 ];
 
 let chapter, part;
 
 for(let i = 0; i < chaptersText.length; i++){
     chapter = document.getElementById(`c${i + 1}`);
-    for(let j = 0; j < partsText.length; j++){
+    for(let j = 0; j < partsText[i].length; j++){
         part = document.getElementById(`c${i + 1}p${j + 1}`);
         if(chapter){
             chapter.textContent = chaptersText[i];
         }
         if(part){
-            part.textContent = partsText[j];
+            part.textContent = partsText[i][j];
         }
     }
 }
