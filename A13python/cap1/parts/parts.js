a6cd1cb9fcf25d840eedbedc2597773b55bd3515
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-07-03 16:49:43 -0500

    Deleted Python

--- A13python/cap1/parts/parts.js
+++
@@ -1,5 +0,0 @@
-if(title.dataset.type == 'part'){
-    const partNumber = title.dataset.number;
-    const part = chapter.parts[partNumber - 1];
-    document.getElementById('part').textContent = `Parte ${partNumber}: ${part.name}`;
-}
